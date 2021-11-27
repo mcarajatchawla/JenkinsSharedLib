ef0345ff971327c44c01a8b521dbd889134e4311
mcarajatchawla <mcarajatchawla@gmail.com>
2021-11-27 20:40:04 +0530

    Sonar call changes

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -2,4 +2,5 @@ def call(String mavenargs="") {
     withMaven(maven: 'Maven-3.5.3') {
          bat "mvn clean install ${mavenargs}"
     }
+    junit 'target/surefire-reports/*.xml'
 }
--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,4 +1,4 @@
-def sonarScan() {
+def call() {
     withCredentials([string(credentialsId: 'sonartoken', variable: 'SonarToken')]) {
         withMaven(maven: 'Maven-3.5.3') {
             bat "mvn sonar:sonar -Dsonar.host.url=${env.SONAR_HOST}  -Dsonar.login=${SonarToken}"
