e9f8129fa0b620a89f0a870514246cef02351fa1
mcarajatchawla <mcarajatchawla@gmail.com>
2021-11-27 21:36:50 +0530

    updated scan methods

---
+++ vars/checkmarxScan.groovy
@@ -0,0 +1,13 @@
+def call() {
+    // step([$class: 'CxScanBuilder',  
+    //     configAsCode: true, 
+    //     credentialsId: 'CheckMarxCredential', 
+    //     exclusionsSetting: 'global', 
+    //     failBuildOnNewResults: false, 
+    //     failBuildOnNewSeverity: 'HIGH', 
+    //     fullScanCycle: 10, 
+    //     projectName: 'MyProject',
+    //     sastEnabled: true, 
+    //     serverUrl: env.CHECKMARX_HOST, 
+    //     vulnerabilityThresholdResult: 'FAILURE', waitForResultsEnabled: true])
+}
---
+++ vars/nexusIQScan.groovy
@@ -0,0 +1,8 @@
+def call() {
+    //   nexusPolicyEvaluation 
+    //     failBuildOnNetworkError: false, 
+    //     iqApplication: 'artifact.war', 
+    //     iqScanPatterns:  [[scanPattern: '**/*.war']], 
+    //     iqStage: 'build', 
+    //     jobCredentialsId: 'NEXUS-CRED'
+}
