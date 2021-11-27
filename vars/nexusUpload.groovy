616df7ae844dbd8bc3bf49340e82562604b57771
mcarajatchawla <mcarajatchawla@gmail.com>
2021-11-27 23:41:35 +0530

    added nexus upload method

---
+++ vars/nexusUpload.groovy
@@ -0,0 +1,15 @@
+def call(String repo) {
+    pom = readMavenPom file: "pom.xml";
+
+    nexusArtifactUploader 
+        artifacts: [[artifactId: pom.artifactId, classifier: '', 
+                    file: 'target/'+pom.artifactId+'-'+pom.version+'.'+pom.packaging, 
+                    type: pom.packaging]], 
+        credentialsId: 'GitHubToken', 
+        groupId: pom.groupId, 
+        nexusUrl: 'localhost:8081', 
+        nexusVersion: 'nexus3', 
+        protocol: 'https', 
+        repository: repo, 
+        version: pom.version
+}
