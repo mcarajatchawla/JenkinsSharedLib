048fe2584a5d7943979bfc3012ccbbca2c110577
mcarajatchawla <mcarajatchawla@gmail.com>
2021-11-27 19:23:17 +0530

    added shared lib methods

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,5 +1,5 @@
 def call(String mavenargs="") {
     withMaven(maven: 'Maven-3.5.3') {
-         sh "mvn clean install ${mavenargs}"
+         bat "mvn clean install ${mavenargs}"
     }
 }
--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,7 +1,7 @@
 def sonarScan() {
     withCredentials([string(credentialsId: 'sonartoken', variable: 'SonarToken')]) {
         withMaven(maven: 'Maven-3.5.3') {
-                sh "mvn sonar:sonar -Dsonar.host.url=${env.SONAR_HOST}  -Dsonar.login=${SonarToken}"
+                bat "mvn sonar:sonar -Dsonar.host.url=${env.SONAR_HOST}  -Dsonar.login=${SonarToken}"
             }
         }
     }
